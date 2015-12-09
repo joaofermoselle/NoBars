8d478e9a3d5d3dcf057ae10f37b43ca4ef6ba7dc
Joao Fermoselle <joao.fermoselle@gmail.com>
2015-12-09 00:07:19 +0000

    Created empty Xcode project

---
+++ NoBars/NoBars/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NoBars
+//
+//  Created by Joao Fermoselle on 09/12/2015.
+//  Copyright © 2015 JRFS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
